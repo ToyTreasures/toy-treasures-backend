f164f0d7065b136426d1c3d83aa1d1b091919149
ziad <ziad.nouh111@gmail.com>
2024-09-07 08:51:22 +0300

    feat: create user model schema using mongoose

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      match: [
+        /^(010|011|012|015)\d{8}$/,
+        "Please enter a valid Egyptian phone number",
+      ],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+      },
+    },
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
