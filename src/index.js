41bfe480f304d5a860aee7b3f5e1fbbeb561dfb7
Eyada <mohamed.ayada.ma@gmail.com>
2024-09-15 14:57:03 +0300

    feat: add register and login routes in auth.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ require("express-async-errors");
 const cors = require("cors");
 
 require("dotenv").config();
-const DB_URI = process.env.DB_URI;
+const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT;
 
 const userRoutes = require("./routes/user.routes");
 const itemRoutes = require("./routes/item.routes");
+const authRoutes = require("./routes/auth.routes");
 
 const UserController = require("./controllers/user.controllers");
 const ItemController = require("./controllers/item.controllers");
+const AuthController = require("./controllers/auth.controller");
 
 const UserRepository = require("./repositories/user.repository");
 const ItemRepository = require("./repositories/item.repository");
@@ -22,6 +24,7 @@ const itemRepository = new ItemRepository();
 
 const userController = new UserController(userRepository);
 const itemController = new ItemController(itemRepository);
+const authController = new AuthController(userRepository);
 
 const app = express();
 
@@ -32,13 +35,14 @@ app.use(express.json());
 
 mainRouter.use("/users", userRoutes(userController));
 mainRouter.use("/items", itemRoutes(itemController));
+mainRouter.use("/auth", authRoutes(authController));
 
 app.use("/api/v1", mainRouter);
 
 app.use(errorHandler); //global error middleware
 
 mongoose
-  .connect(DB_URI)
+  .connect(DB_URL)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`server is listening on port ${PORT}`);
