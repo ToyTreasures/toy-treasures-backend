b419da55e7c7e8a821bd5091c650d25d1f33c875
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-18 22:05:31 +0200

    modify auth controller to handle validation by joi

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,8 @@ const util = require("util");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSignAsync = util.promisify(jwt.sign);
+const { createUserSchema } = require("../utils/validation/users.validation");
+require("dotenv").config();
 
 class AuthController {
   userRepository;
@@ -11,14 +13,20 @@ class AuthController {
   }
 
   async register(userData) {
-    //joi validation on userData(req.body) first
-    //validation in case like this could be put in pre"save" script in the user model
-    const existingUser = await this.userRepository.getUserByEmail(
-      userData.email
-    );
-    if (existingUser) throw new CustomError("Email already exists", 409);
-    const user = await this.userRepository.createUser(userData);
-    return user;
+    try {
+      const { error, value } = createUserSchema.validate(userData, { abortEarly: false });
+      if (error) {
+        const errorMessages = error.details.map((detail) => detail.message);
+        throw new CustomError(errorMessages.join(", "), 400);
+      }
+      const existingUser = await this.userRepository.getUserByEmail(userData.email);
+      if (existingUser) throw new CustomError("Email already exists", 409);
+      const user = await this.userRepository.createUser(value);
+      return user;
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      throw new CustomError(error.message, 500);
+    }
   }
 
   async login(userData) {
@@ -27,11 +35,9 @@ class AuthController {
     if (!userData.password) throw new CustomError("Password is required", 400);
     const isMatched = await bcrypt.compare(userData.password, user.password);
     if (isMatched) {
-      const token = await jwtSignAsync(
-        { userId: user.id },
-        process.env.JWT_SECRET,
-        { expiresIn: "1d" }
-      );
+      const token = await jwtSignAsync({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
       return { token, user };
     } else {
       throw new CustomError("invalid email or password", 400);
