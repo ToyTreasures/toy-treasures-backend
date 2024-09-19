af29e7e133c9212b492e3f585bfe0531100845e8
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-19 07:37:51 +0200

    add schema of validation using joi to make validation when user register

--- src/utils/validation/users.validation.js
+++ src/utils/validation/users.validation.js
@@ -1,14 +1,21 @@
-// validations/UserValidation.js
 const Joi = require("joi");
 
 const createUserSchema = Joi.object({
-  name: Joi.string().required().min(3).max(30),
+  name: Joi.string().required().min(3).max(30).messages({
+    "string.min": "Name must be at least 3 characters long",
+    "string.max": "Name cannot exceed 30 characters",
+    "any.required": "Name is required",
+  }),
   email: Joi.string()
     .email()
     .required()
-    .pattern(new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")),
+    .pattern(new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"))
+    .message("Please enter a valid email address"),
   password: Joi.string().min(8).required(),
-  phoneNumber: Joi.string().required().pattern(new RegExp("^(010|011|012|015)\\d{8}$")),
+  phoneNumber: Joi.string()
+    .required()
+    .pattern(new RegExp("^(010|011|012|015)\\d{8}$"))
+    .message("Please enter a valid Egyptian phone number"),
   address: Joi.string().required(),
   role: Joi.string().valid("user", "admin").default("user"),
 });
