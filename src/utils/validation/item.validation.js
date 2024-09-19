4143fabd32e2d6fff748ba5f39a60f7418e40168
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-19 07:36:42 +0200

    add schema of validation using joi

--- src/utils/validation/item.validation.js
+++ src/utils/validation/item.validation.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 
 const createItemSchema = Joi.object({
   name: Joi.string().required(),
-  description: Joi.string().max(500).optional(),
+  description: Joi.string().max(500).required(),
   price: Joi.number().min(1).required(),
   condition: Joi.string().required(),
-  isAvailableForSwap: Joi.boolean().default(false),
+  isAvailableForSwap: Joi.boolean().default(false).required(),
   ownerId: Joi.string()
     .custom((value, helpers) => {
       if (!mongoose.Types.ObjectId.isValid(value)) {
@@ -17,19 +17,12 @@ const createItemSchema = Joi.object({
     .optional(),
 });
 
-const updateItemSchema = createItemSchema.fork(["name", "price", "condition"], (schema) =>
-  schema.optional()
+const updateItemSchema = createItemSchema.fork(
+  ["name", "price", "condition", "description"],
+  (schema) => schema.optional()
 );
-
-const paginatedItemsSchema = Joi.object({
-  page: Joi.number().integer().min(1).default(1),
-  limit: Joi.number().integer().min(1).default(12),
-  condition: Joi.string().optional(),
-  isAvailableForSwap: Joi.boolean().optional(),
-});
 
 module.exports = {
   createItemSchema,
   updateItemSchema,
-  paginatedItemsSchema,
 };
