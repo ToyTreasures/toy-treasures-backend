4f761a0eb4cfefe8282debc07d6d866bf77a0ef5
ziad <ziad.nouh111@gmail.com>
2024-09-23 14:48:56 +0300

    feat: add category model

---
+++ src/models/category.model.js
@@ -0,0 +1,29 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const categorySchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    description: {
+      type: String,
+      maxlength: 500,
+    },
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
+    thumbnail: {
+      type: String,
+      required: [true, "Thumbnail is required"],
+    },
+    thumbnailFileId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category = model("Category", categorySchema);
+
+module.exports = Category;
