a3aff6f831b248c2142a2632f6018ca9dc9aed0a
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-27 09:19:46 +0200

    test: add tests for fetching user's items01

--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -7,12 +7,7 @@ const router = express.Router();
 const itemRouter = (itemController) => {
   router.get("/", async (req, res) => {
     const { page, limit, filters, search } = req.query;
-    const { itemsNumber, pagesNumber, items } = await itemController.getAllItems(
-      page,
-      limit,
-      filters,
-      search
-    );
+    const { itemsNumber, pagesNumber, items } = await itemController.getAllItems(page, limit, filters, search);
     res.status(200).send({
       success: "All items fetched successfully",
       items,
@@ -26,47 +21,35 @@ const itemRouter = (itemController) => {
     res.status(200).send({ success: "Item fetched successfully", item });
   });
 
-  router.post(
-    "/",
-    auth,
-    upload.fields([{ name: "thumbnail", maxCount: 1 }]),
-    async (req, res) => {
-      const ownerId = req.user._id;
-      const thumbnail = req.files;
-      const item = { ...req.body, thumbnail, ownerId };
+  router.get("/user/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const items = await itemController.getUserItems(userId);
+    res.status(200).send({ success: "User items fetched successfully", items });
+  });
 
-      const createdItem = await itemController.createItem(item);
+  router.post("/", auth, upload.fields([{ name: "thumbnail", maxCount: 1 }]), async (req, res) => {
+    const ownerId = req.user._id;
+    const thumbnail = req.files;
+    const item = { ...req.body, thumbnail, ownerId };
 
-      res
-        .status(200)
-        .send({ success: "Item created successfully", item: createdItem });
+    const createdItem = await itemController.createItem(item);
+
+    res.status(200).send({ success: "Item created successfully", item: createdItem });
+  });
+
+  router.patch("/:id", auth, upload.fields([{ name: "thumbnail", maxCount: 1 }]), async (req, res) => {
+    const { id: itemId } = req.params;
+    const { _id: userId } = req.user;
+    const itemData = { ...req.body };
+
+    if (req.files && req.files.thumbnail && req.files.thumbnail.length > 0) {
+      const thumbnailFile = req.files.thumbnail[0];
+      itemData.thumbnail = thumbnailFile;
     }
-  );
 
-  router.patch(
-    "/:id",
-    auth,
-    upload.fields([{ name: "thumbnail", maxCount: 1 }]),
-    async (req, res) => {
-      const { id: itemId } = req.params;
-      const { _id: userId } = req.user;
-      const itemData = { ...req.body };
-
-      if (req.files && req.files.thumbnail && req.files.thumbnail.length > 0) {
-        const thumbnailFile = req.files.thumbnail[0];
-        itemData.thumbnail = thumbnailFile;
-      }
-
-      const updatedItem = await itemController.updateItem(
-        itemId,
-        userId,
-        itemData
-      );
-      res
-        .status(200)
-        .send({ success: "Item updated successfully", updatedItem });
-    }
-  );
+    const updatedItem = await itemController.updateItem(itemId, userId, itemData);
+    res.status(200).send({ success: "Item updated successfully", updatedItem });
+  });
 
   router.delete("/:id", auth, async (req, res) => {
     const { id: itemId } = req.params;
